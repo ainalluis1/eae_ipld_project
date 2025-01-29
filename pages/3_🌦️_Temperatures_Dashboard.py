cb7ced3bf536eb2703086a12a994654b6e617275
ainalluis1 <uabaina112@gmail.com>
2025-01-29 14:35:17 +0100

    Update 3_🌦️_Temperatures_Dashboard.py

--- pages/3_🌦️_Temperatures_Dashboard.py
+++ pages/3_🌦️_Temperatures_Dashboard.py
@@ -118,7 +118,7 @@ st.header("Comparing the Temperatures of the Cities")
 
 if unique_cities_list is not None:
     # Getting the list of cities to compare from the user
-    selected_cities = st.multiselect("Select the cities to compare:", unique_countries_list, default=["Buenos Aires", "Dakar"], max_selections=4)
+    selected_cities = st.multiselect("Select the cities to compare:", unique_cities_list, default=["Buenos Aires", "Dakar"], max_selections=4)
 
     cols2 = st.columns([6, 1, 6])
 
